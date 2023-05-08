44220fec6e9720d22d7b413336df9ba8419d1e5d
Adrian Smith <smith.adriane@gmail.com>
2023-05-08 12:54:31 -0700

    fixing path in bootstrap.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,16 +3,14 @@
 set -e
 set -x
 
-
-
 #Downloading the latest Miniconda installer for macOS. Your architecture may vary.
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-$CONDA_PLATFORM-$ARCH.sh -o miniconda.sh
 
 # silent install
 bash ./miniconda.sh -b
 
-~/miniconda/bin/conda create -n cljonda-meta
-~/miniconda/bin/conda install -n cljonda-meta conda
+~/miniconda3/bin/conda create -n cljonda-meta
+~/miniconda3/bin/conda install -n cljonda-meta conda
 
 
 
