bca60310890e262cb4b0259692dfdd0d0fb4d0b1
Adrian Smith <smith.adriane@gmail.com>
2023-06-24 11:53:30 -0700

    Add deployment for libretro snes9x.

---
+++ src/com/phronemophobic/cljonda/libretro_snes9x.clj
@@ -0,0 +1,166 @@
+(ns com.phronemophobic.cljonda.libretro_snes9x
+  (:require [clojure.java.shell :as sh]
+            [clojure.tools.build.api :as b]
+            [clojure.java.io :as io]
+            deps-deploy.deps-deploy
+            [com.phronemophobic.cljonda.core
+             :as cljonda]
+            [clojure.set :as set]
+            [clojure.string :as str]
+            [clojure.pprint :refer [pprint]])
+  (:import java.nio.file.Files))
+
+(defn delete-tree
+  "Deletes a file or directory."
+  [f & [silently]]
+  (when (.isDirectory f)
+    (doseq [childf (.listFiles f)]
+      (delete-tree childf silently)))
+  (io/delete-file f silently))
+
+(def build-dir (io/file "build"))
+
+(defn sanitize-version [version]
+  (str/replace version #"[^a-zA-Z0-9+_.]" "_"))
+
+(defn create-cljonda-jar [package-name version lib-files ]
+  (let [jar-file (io/file
+                  build-dir
+                  (str package-name ".jar"))
+
+        package-build-dir
+        (doto (io/file build-dir package-name)
+          (.mkdirs))
+
+        target-dir (io/file package-build-dir
+                            (cljonda/system-arch))]
+    (do (delete-tree package-build-dir)
+        (.mkdir package-build-dir))
+    
+    (try
+      ;; copy dylibs to the right spot
+      (doseq [f lib-files
+              :when (not (Files/isSymbolicLink (.toPath f)))
+              :let [target-file (io/file target-dir
+                                         (.getName f))
+                    target-path (.getAbsolutePath target-file)]]
+        (b/copy-file {:src (.getCanonicalPath f)
+                      :target target-path}))
+
+      ;; src dir with namespace
+      (let [lib (symbol "com.phronemophobic.cljonda" (str package-name "-" (cljonda/system-arch)))
+            class-dir (.getAbsolutePath package-build-dir)]
+        (b/write-pom {:lib lib
+                      :version version
+                      :class-dir (.getAbsolutePath package-build-dir)
+                      :basis {}})
+        (b/jar {:class-dir class-dir
+                :jar-file (.getAbsolutePath jar-file)})
+        {:jar-file (.getAbsolutePath jar-file)
+         :lib lib
+         :pom-file (b/pom-path {:lib lib :class-dir class-dir})})
+      
+      (finally
+        #_(delete-tree package-build-dir)
+        ))))
+
+(defn deploy-jar-pom [{:keys [jar-file pom-file]}]
+  (try
+    (println "deploying" jar-file)
+    (deps-deploy.deps-deploy/deploy
+     {:installer :remote
+      :artifact jar-file
+      :pom-file pom-file})
+    (catch Exception e
+      (if-not (str/includes? (ex-message e) "redeploying non-snapshots is not allowed")
+        (throw e)
+        (println "This release was already deployed.")))))
+
+
+(defn assert-sh [& args]
+  (println "running " args)
+  (let [{:keys [exit out err]} (apply sh/sh args)]
+    (println "ran " args)
+    (when (not (zero? exit))
+      (throw (ex-info "Shell Command Error"
+                      {:type ::shell-command-error
+                       :exit exit
+                       :args args
+                       :out out
+                       :err err})))))
+
+(defn prep-libretro [commit]
+  (doto (io/file build-dir)
+    (.mkdirs))
+  (let [lib-dir (io/file build-dir "libretro-super")
+        lib-path (.getCanonicalPath lib-dir)]
+    (when (not (.exists lib-dir))
+      (assert-sh "git" "clone" "https://github.com/libretro/libretro-super"
+                 :dir (.getCanonicalPath (io/file lib-dir "../"))))
+    
+    (assert-sh "git" "checkout" commit
+               :dir lib-path)
+
+    (assert-sh (-> (io/file lib-dir "libretro-fetch.sh")
+                   (.getCanonicalPath))
+               "snes9x"
+               :dir lib-path)
+
+    (assert-sh (-> (io/file lib-dir "libretro-build.sh")
+                   (.getCanonicalPath))
+               "snes9x"
+               :dir lib-path)
+    
+    (let [target-file (io/file lib-dir (str "libretro_snes9x." (cljonda/shared-lib-suffix)))
+          target-path (.getCanonicalPath target-file) ]
+      (b/copy-file {:src (.getCanonicalPath (io/file lib-dir "libretro-snes9x" "libretro" (str "snes9x_libretro." (cljonda/shared-lib-suffix)))) 
+                    :target target-path})
+      [target-file])))
+
+(defn jar-libretro [version lib-files]
+  (create-cljonda-jar  "libretro_snes9x"
+                       version
+                       lib-files)
+)
+
+(defn deploy-libretro [commit]
+  (let [lib-files (prep-libretro commit)
+        deploy-info (jar-libretro (str commit "-SNAPSHOT")
+                                  lib-files)]
+    (deploy-jar-pom deploy-info)))
+
+(defn -main [commit]
+  (try
+    (deploy-libretro commit)
+    (catch clojure.lang.ExceptionInfo e
+      (let [data (ex-data e)]
+        (case (:type data)
+
+          ::shell-command-error
+          (let [{:keys [exit args out err]} data]
+            (println "args: " (pr-str args))
+            (println "Exit: " exit)
+            (println "---- out -----")
+            (println out)
+            (println "---- err -----")
+            (println err)
+            (println "--------------")
+            (throw e))
+
+          ;; else
+          (println e)
+          (throw e)
+          ))
+      )
+    (catch Throwable e
+      (println e)
+      (throw e))))
+
+(comment
+  (prep-libretro "ba788307f326419100f2369ff54912bda20f9026")
+  (-main "ba788307f326419100f2369ff54912bda20f9026")
+
+
+  
+
+  )
