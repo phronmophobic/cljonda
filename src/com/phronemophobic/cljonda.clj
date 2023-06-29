dac49602c3d4a0836e5d920ce7f21170d0796405
Adrian Smith <smith.adriane@gmail.com>
2023-06-28 19:44:55 -0700

    bumping version to 0.9.2.

--- src/com/phronemophobic/cljonda.clj
+++ src/com/phronemophobic/cljonda.clj
@@ -135,7 +135,7 @@
 (defn sanitize-version [version]
   (str/replace version #"[^a-zA-Z0-9+_.]" "_"))
 
-(def cljonda-version "0.9.1")
+(def cljonda-version "0.9.2")
 
 (defn create-cljonda-jar [prefix versions package]
   (let [package-name (get package "name")]
