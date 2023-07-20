d076dde7676ee6786667e3c435c5b069bdd72dcc
Adrian Smith <smith.adriane@gmail.com>
2023-07-19 17:17:28 -0700

    add more stuff to bootstrap.

--- scripts/bootstrap_llama.sh
+++ scripts/bootstrap_llama.sh
@@ -9,6 +9,16 @@ if [[ $(uname) == "Darwin" ]]; then
 else
     # linux
     sudo apt-get update -y
-    sudo apt-get install cmake3 cmake3-data -y
+    sudo apt-get install build-essential software-properties-common -y
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo apt-get update -y
+    sudo apt-get install build-essential software-properties-common -y
+    sudo apt-get update
+    sudo apt-get install gcc-9 g++-9 -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+    sudo update-alternatives --config gcc 
+    sudo apt-get install fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
+
+
 fi
 
