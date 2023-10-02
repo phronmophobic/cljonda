890b072861b4ce5b0142db3ca04ef286f76e98d7
Adrian Smith <smith.adriane@gmail.com>
2023-10-01 23:56:43 -0700

    update llama.

--- src/com/phronemophobic/cljonda/libllama.clj
+++ src/com/phronemophobic/cljonda/libllama.clj
@@ -158,7 +158,7 @@
 (defn -main [commit]
   (try
     (let [lib-files (prep-llama commit)
-          version (str commit "-SNAPSHOT")
+          version commit
           deploy-info (jar-llama version
                                  lib-files)]
       (deploy-jar-pom deploy-info))
