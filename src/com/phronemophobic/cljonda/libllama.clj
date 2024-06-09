090d95b90d34467899bd88eb57b166bbbba6a520
Adrian Smith <smith.adriane@gmail.com>
2024-06-08 20:42:22 -0700

    bump libllama commit to b3040.

--- src/com/phronemophobic/cljonda/libllama.clj
+++ src/com/phronemophobic/cljonda/libllama.clj
@@ -190,7 +190,7 @@
   (deploy-llama "c3f197912f1ce858ac114d70c40db512de02e2e0")
   (-main "c3f197912f1ce858ac114d70c40db512de02e2e0")
 
-  (let [commit "b2291"
+  (let [commit "b3040"
         lib-files (prep-llama commit )
         deploy-info (jar-llama commit
                                lib-files)]
