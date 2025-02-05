f83b384807909a0187bc6bd5b6050f7e85261e63
Adrian Smith <smith.adriane@gmail.com>
2025-02-04 23:41:59 -0800

    promoting libllama from snapshot.

--- src/com/phronemophobic/cljonda/libllama.clj
+++ src/com/phronemophobic/cljonda/libllama.clj
@@ -169,7 +169,7 @@
 (defn -main [commit]
   (try
     (let [lib-files (prep-llama commit)
-          version (str commit "-SNAPSHOT")
+          version (str commit #_"-SNAPSHOT")
           deploy-info (jar-llama version
                                  lib-files)]
       (deploy-jar-pom deploy-info))
